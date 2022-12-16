0badbcc0589341e19252a8290e8b6554dbe7c21e
braden wales <bra.wales00@gmail.com>
2022-12-15 18:51:26 -0600

    added schemas

--- models/user.js
+++ models/user.js
@@ -1,8 +1,27 @@
 const mongoose = require('mongoose') ;
 
 const userSchema = new mongoose.Schema({
-  username:
-  email:
-  thoughts:
-  friends:  
-})
+  username: { type: String, unique: true , required: true} ,
+  email: { type: String, unique: true , required: true} ,
+  thought: [{ type: Objectid, ref: 'Thought'}] ,
+  friends: [{ type: Objectid, ref: 'User'}] ,
+});
+
+const thoughtSchema = new mongoose.Schema({
+  thoughtText: {type: String, required: true , maxLength: 280 } ,
+  createdAt: {
+          type: Date,
+          get: (date) => timeSince(date) ,
+  } ,
+  username: { type: String , required: true} ,
+}) ;
+
+const reactionSchema = new mongoose.Schema({
+  reactionid: {type: ObjectId , default: objectId.new} ,
+  reactionBody: { type: String, required: true, maxLength: 280} ,
+  username: { type: String, required: true} ,
+  createdAt: {
+    type: Date,
+    get: (date) => timeSince(date) ,
+} ,
+}) ;
